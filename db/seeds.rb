d85a9b0585f498196e0e723a669d414f57fda585
Alex R <jpalre@outlook.com>
2019-05-27 18:31:54 +0900

    seed added for users trips reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'deleting boockings'
+# Bocking.destroy_all
+puts 'deleting trips'
+# Trip.destroy_all
+puts 'deleting reviews'
+# Review.destroy_all
+puts 'creating ingredients...'
+
+5.times do
+  user = User.create!(
+    name: Faker::Name.unique.name,
+    email: Faker::Internet.email,
+    password: 'secret',
+    )
+  2.times do
+    trip_in_days = rand(1..5)
+    Trip.create!(
+      start_date: Date.today + trip_in_days,
+      end_date: Date.today + trip_in_days,
+      start_point: Faker::Address.city,
+      end_point: Faker::Address.city,
+      description: 'blabla-trip',
+      seats_available: rand(1..3),
+      user: user,
+      )
+  end
+    4.times do
+    Review.new(
+      rating: rand(1..3),
+      comment: Faker::Hipster.words(4),
+      reviewed:
+      )
+  end
+end
