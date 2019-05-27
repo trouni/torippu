c50431c5140b7b93cc627545b1a12b759c6f34e6
Trouni Tiet <trouni@gmail.com>
2019-05-27 16:47:29 +0900

    added devise and user model > run db:migrate

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = false
-
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     config.cache_store = :null_store
   end
 
