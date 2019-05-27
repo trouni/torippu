9e2c7fb92a4364a8986fd2e4ca169151470488f8
Trouni Tiet <trouni@gmail.com>
2019-05-27 22:58:52 +0900

    configured carrierwave and mounted uploaded to user photo > rails db:migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips_as_driver, source: :trips, foreign_key: :driver_id
   has_many :trips_as_passenger, through: :bookings, foreign_key: :passenger_id
   has_many :reviews, foreign_key: :reviewed_id
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :photo, PhotoUploader
 end
