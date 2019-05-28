41cfc92ba612c928aefaa2f4d4bb2544b3acd313
Trouni Tiet <trouni@gmail.com>
2019-05-28 17:57:53 +0900

    search by origin - destination

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,14 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show]
 
   def index
-    @trips = policy_scope(Trip)
+    if params[:destination]
+      destination = params[:destination]
+      origin = params[:origin] || "Tokyo"
+      date = params[:date]
+      @trips = policy_scope(Trip).where(end_point: destination, start_point: origin)
+    else
+      @trips = policy_scope(Trip)
+    end
   end
 
   def show
