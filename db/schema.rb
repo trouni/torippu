95d47d41014468ebb21d271c17bcf90e18ada32e
Alex R <jpalre@outlook.com>
2019-05-28 11:48:14 +0900

    added trips controller and index view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_083951) do
+ActiveRecord::Schema.define(version: 2019_05_27_135626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_083951) do
     t.datetime "updated_at", null: false
     t.index ["driver_id"], name: "index_trips_on_driver_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,8 +59,14 @@ ActiveRecord::Schema.define(version: 2019_05_27_083951) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "trips"
+  add_foreign_key "bookings", "users", column: "passenger_id"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users", column: "reviewed_id"
+  add_foreign_key "trips", "users", column: "driver_id"
 end
