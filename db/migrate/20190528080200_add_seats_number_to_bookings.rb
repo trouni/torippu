9b6d33e595704b525fd6959128422e2478ca74f2
Saad Amrani Joutey <amrani.saad@gmail.com>
2019-05-28 18:12:47 +0900

    add_colmun correction sur booking

--- db/migrate/20190528080200_add_seats_number_to_bookings.rb
+++ db/migrate/20190528080200_add_seats_number_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddSeatsNumberToBookings < ActiveRecord::Migration[5.2]
   def change
-    add column :bookings, :seats_number, :integer, default: 1
+    add_column :bookings, :seats_number, :integer, default: 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_055839) do
+ActiveRecord::Schema.define(version: 2019_05_28_080200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_055839) do
     t.bigint "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "seats_number", default: 1
     t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
     t.index ["trip_id"], name: "index_bookings_on_trip_id"
   end
@@ -28,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_055839) do
     t.bigint "booking_id"
     t.bigint "user_id"
     t.string "comment"
-    t.float "rating"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
