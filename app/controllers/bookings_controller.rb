30e1d69557c46260644311df1f35a98c74380af6
Saad Amrani Joutey <amrani.saad@gmail.com>
2019-05-28 11:36:33 +0900

    adding bookings controller with create

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  def create
+    @trip = Trip.find(params[:trip_id])
+    @booking = Booking.new(booking_params)
+    @booking.passenger = current_user
+    @booking.trip = @trip
+    if @booking.save
+      redirect_to trip_path(@trip)
+    else
+      render "trips/show"
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit( :approved)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
   devise_for :users
   root to: 'pages#home'
   resources :trips, only: [:index, :show, :new, :create, :edit, :update] do
