9c45f13773fa885367cf75a430ade8a2a5158424
Alex R <jpalre@outlook.com>
2019-05-30 10:44:54 +0900

    just started with validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :passenger, class_name: "User"
   belongs_to :trip
+  validates :seats_number, presence: true
+  validates_numericality_of :seats_number, less_than: -> (trip) { trip.seats_available }
+  validates_numericality_of :seats_number, greater_than: 0
 end
