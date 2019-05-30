d064151b1675a51bfee081937786c8da21c2bf0e
eugene sia <teamkronos2019@gmail.com>
2019-05-30 18:48:17 +0900

    edited review-controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :trip
   belongs_to :reviewee, class_name: "User"
   belongs_to :reviewer, class_name: "User"
-  validates :comment, presence: true, uniqueness: true
+  validates :comment, presence: true
   validates :rating, inclusion: { in: 0..5 }
 end
