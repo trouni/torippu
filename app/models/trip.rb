e6a1909b95385ff1dd4247d527733d6413a916f3
Trouni Tiet <trouni@gmail.com>
2019-05-30 13:17:14 +0900

    added algolia places to search form

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,14 @@ class Trip < ApplicationRecord
   belongs_to :driver, class_name: "User"
   has_many :bookings
 
+  def start_date
+    start_time.to_date
+  end
+
+  def end_date
+    end_date.to_date
+  end
+
   def booking?(user)
     bookings.each do |booking|
       return true if booking.passenger == user
