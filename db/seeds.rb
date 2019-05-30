6725f27f4923909f8f8f0a7ec11be4f8ec75d073
Trouni Tiet <trouni@gmail.com>
2019-05-30 15:22:01 +0900

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require 'faker'
 
-CITIES = %w[Tokyo Osaka Kyoto Kobe Fukuoka]
+CITIES = [
+  'Tokyo, 東京都, Japan',
+  'Osaka, 大阪府, Japan',
+  'Fukuoka, 福岡県, Japan',
+  'Kyōto, 京都府, Japan'
+]
 
 USERS = [
   {
@@ -28,15 +33,16 @@ USERS = [
     password: "secret",
   },
 ]
-puts 'deleting bookings'
+puts 'Deleting bookings...'
 Booking.destroy_all
-puts 'deleting trips'
+puts 'Deleting trips...'
 Trip.destroy_all
-puts 'deleting users'
+puts 'Deleting users...'
 User.destroy_all
 # puts 'deleting reviews'
 # Review.destroy_all
 
+puts 'Creating users and trips...'
 USERS.each do |user_hash|
   user = User.create!(user_hash)
   user.remote_photo_url = user_hash[:photo]
@@ -66,6 +72,7 @@ USERS.each do |user_hash|
   # end
 end
 
+puts 'Creating bookings...'
 Trip.all.each do |trip|
   rand(1..trip.seats_available).times do |variable|
     Booking.create!(
