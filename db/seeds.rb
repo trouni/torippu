14c515c407322c460448f513ebd704b3ca0110b2
Trouni Tiet <trouni@gmail.com>
2019-05-31 16:41:21 +0900

    fixed cards bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,56 +30,56 @@ USERS = [
   },
 ]
 
-# puts 'deleting reviews'
-# Review.destroy_all
-# puts 'Deleting bookings...'
-# Booking.destroy_all
-# puts 'Deleting trips...'
-# Trip.destroy_all
-# puts 'Deleting users...'
-# User.destroy_all
+puts 'deleting reviews'
+Review.destroy_all
+puts 'Deleting bookings...'
+Booking.destroy_all
+puts 'Deleting trips...'
+Trip.destroy_all
+puts 'Deleting users...'
+User.destroy_all
 
 
-# i = 0
-# 100.times do
-#   name = Faker::Name.name.split(" ").first
-#   username = User.new(
-#     username: name,
-#     email: name + i.to_s + "@me.com",
-#     password: "secret")
-#   i += 1
-#   username.remote_photo_url = "http://lorempixel.com/200/200/people/"
-#   username.save!
-#   print "#"
-#   # create_trips(username)
-# end
+i = 0
+100.times do
+  name = Faker::Name.name.split(" ").first
+  username = User.new(
+    username: name,
+    email: name + i.to_s + "@me.com",
+    password: "secret")
+  i += 1
+  username.remote_photo_url = "http://lorempixel.com/200/200/people/"
+  username.save!
+  print "#"
+  # create_trips(username)
+end
 
-# puts ""
-# puts 'Creating users...'
-# USERS.each do |user_hash|
-#   user = User.create!(user_hash)
-#   user.remote_photo_url = user_hash[:photo]
-#   user.save!
-#   print "#"
-#   # create_trips(user)
-# end
+puts ""
+puts 'Creating users...'
+USERS.each do |user_hash|
+  user = User.create!(user_hash)
+  user.remote_photo_url = user_hash[:photo]
+  user.save!
+  print "#"
+  # create_trips(user)
+end
 
-# puts "Creating trips..."
-# addresses["from"].each do |address|
-#   start_time = DateTime.now - rand(1..60) + rand(1..60) + rand # start date between now and 1 month from now
-#   end_time = start_time + rand(1..10) / 24.0 # trip duration between 1 and 10 hours
-#   Trip.create!(
-#     start_time: start_time,
-#     end_time: end_time,
-#     start_point: address,
-#     end_point: addresses["to"].sample,
-#     description: Faker::TvShows::HowIMetYourMother.quote,
-#     seats_available: rand(1..4),
-#     driver: User.all.sample,
-#     price: 1500 + rand(1..70) * 100
-#   )
-#   print "#"
-# end
+puts "Creating trips..."
+addresses["from"].each do |address|
+  start_time = DateTime.now - rand(1..60) + rand(1..60) + rand # start date between now and 1 month from now
+  end_time = start_time + rand(1..10) / 24.0 # trip duration between 1 and 10 hours
+  Trip.create!(
+    start_time: start_time,
+    end_time: end_time,
+    start_point: address,
+    end_point: addresses["to"].sample,
+    description: Faker::TvShows::HowIMetYourMother.quote,
+    seats_available: rand(1..4),
+    driver: User.all.sample,
+    price: 1500 + rand(1..70) * 100
+  )
+  print "#"
+end
 
 
 puts 'Creating bookings...'
